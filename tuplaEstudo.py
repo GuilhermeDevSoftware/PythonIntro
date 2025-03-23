1e8787f87bf69ccbfd506d3ce47a688d460fb81a
Guilherme Costa <guizeracostasilva@outlook.com>
2025-03-23 08:52:09 -0300

    Estudo de tupla

---
+++ tuplaEstudo.py
@@ -0,0 +1,12 @@
+filmesTupla = ("Batman", "Zatura", "Rambo", "Joker", "Branca de Neve")
+
+print(type(filmesTupla))
+
+#Buscando elementos
+print(filmesTupla[:3])
+
+#Buscando o ultimo
+print(filmesTupla[-1])
+
+#Buscando posicao
+print(filmesTupla.index("Joker"))
